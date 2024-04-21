83c580b5c610d6b95e7680ef4ccebae221e8989c
Horatiu Bolovan <93126326+Aciroh@users.noreply.github.com>
2024-04-21 11:24:41 +0300

    [Add Just_RO Models] Add models and documentation, and join + union with scj (#4)
    * [Add Just_RO Models] Add models and documentation, and join + union with scj
    
    * [Add Just_RO models] Merge all schema files into one

--- dbt/models/dosare/models/stg_dosare.sql
+++ dbt/models/dosare/models/stg_dosare.sql
@@ -1,7 +1,26 @@
 with source as (
-    select *
-    from {{source('dosare_raw', 'dosare')}}
+    select 
+        scj.numar as numar,
+        scj.numarvechi as numar_vechi,
+        scj.obiect as obiect,
+        scj.datainitiala as data_initiala,
+        stadiuprocesual as stadiu_procesual,
+        categoriecaz as categorie_caz,
+        stadiulprocesualcombinat as stadiul_procesual_combinat,
+        departament
+    from {{source('dosare_raw', 'scj')}} scj
+    union all
+    select
+        numar,
+        numar_vechi,
+        obiect,
+        data_initial as data_initiala,
+        null as stadiu_procesual,
+        null as categorie_caz,
+        null as stadiul_procesual_combinat,
+        null as departament
+    from {{source('dosare_raw', 'dosare_just_ro')}} just
 )
 
-select *
+select distinct *
 from source
