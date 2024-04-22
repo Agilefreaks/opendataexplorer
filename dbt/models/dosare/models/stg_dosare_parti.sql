dab24c201b0b647a996362b6d773f1c292995923
Horatiu Bolovan <93126326+Aciroh@users.noreply.github.com>
2024-04-22 10:56:27 +0300

    [Add Just Ro Models] Cast date columns to date type and coalesce 'numar' and 'numar_vechi' to same column

--- dbt/models/dosare/models/stg_dosare_parti.sql
+++ dbt/models/dosare/models/stg_dosare_parti.sql
@@ -1,23 +1,15 @@
 with source as (
-    select 
-        just.nume,
-        just.calitate_parte,
-        just.numar,
-        scj.data as data,
-        scj.calitateaprocesualaanterioara as calitate_parte_anterioara
-    from {{source('dosare_raw', 'parti_dosare_just_ro')}} just
-    left join {{source('dosare_raw', 'scj_parti')}} scj
-        on trim(lower(just.calitate_parte)) = trim(lower(scj.calitateaprocesualacurenta))
-        and trim(lower(just.nume)) = trim(lower(scj.nume))
-    union all
     select
         nume,
         calitateaprocesualacurenta as calitate_parte,
-        null as numar,
-        data,
+        coalesce(numar, numarvechi) as numar_1,
+        to_date(parte.data, 'YYYY-MM-DD') as data,
         calitateaprocesualaanterioara as calitate_parte_anterioara
-    from {{source('dosare_raw', 'scj_parti')}}
+    from {{source('dosare_raw', 'scj_parti')}} parte
+    left join {{source('dosare_raw', 'scj')}} dosar
+        on parte._airbyte_scj_hashid = dosar._airbyte_scj_hashid
 )
 
 select distinct *
 from source
+
